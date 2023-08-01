9bb68faba6d11322cbab6dfd0bb8382d12d15c3f
sergxlove <sergxlove@mail.ru>
2023-08-01 12:56:45 +0600

    version_2.2

---
+++ MyForm3.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace casinowinForms {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm3
+	/// </summary>
+	public ref class MyForm3 : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm3(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm3()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MyForm3
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1075, 614);
+			this->Name = L"MyForm3";
+			this->Text = L"MyForm3";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
